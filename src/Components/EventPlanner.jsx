e17ce3e20baf5cdb576d9c55a8f4ae6336e2fc32
AbhishekPattnaik124 <abhishekpattnaik612@gmail.com>
2024-12-22 21:54:45 +0530

    initial commit

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './EventPlanner.css'; // Import CSS file for styling
+import Footer from './Footer';
+
 
 const EventPlanner = () => {
     return (
@@ -7,6 +9,77 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+        <section className='description'>
+                <p>
+                    Plan and organize your events effortlessly with Event Planner. From
+                    birthdays to corporate meetings, we've got you covered.
+                </p>
+                <button className='get-started-button'>getstarted</button>
+        </section>
+        <section className='events_categories'>
+            <ul>
+                <h2>Social Events:</h2>
+                <li>Birthday parties</li>
+                <li>Anniversary celebration</li>
+                <li>Wedding receptions</li>
+                <li>Baby Showers</li>
+                <li>Graduation parties</li>
+                <li>Family reunions</li>
+            </ul>
+            <ul>
+                <h2>Entertainment Events:</h2>
+                <li>Concerts</li>
+                <li>Music festivals</li>
+                <li>Film screening</li>
+                <li>Comedy Shows</li>
+                <li>Art exhibitions</li>
+                <li>Cultural events</li>
+            </ul>
+            <ul>
+                <h2>Community Events:</h2>
+                <li>Fundrasing events</li>
+                <li>Charity Galas</li>
+                <li>Volunteer drives</li>
+                <li>Neighbourhood block parties</li>
+                <li>Community festibles</li>
+                <li>Cultural celebration</li>
+            </ul>
+        </section>
+        <section className='features'>
+            <h2>Features</h2>
+            <ul>
+                <li>Easy event creation and management</li>
+                <li>Custoizable event templates</li>
+                <li>Guest list management</li>
+                <li>Real-time collaboration</li>
+                <li>Reminders and notification</li>
+            </ul>
+        </section>
+        <section className='testimonials'>
+            <h2>Testimonials</h2>
+            <div className='testimonial'>
+            <p>"Event Planner made organizing my wedding a breeze. Highly recommended!"</p>
+            <p className="author">- Emily Johnson</p>
+            </div>
+            <div>
+            <p>"I use Event Planner for all my corporate events. It saves me so much time and effort!"</p>
+            <p className="author">- John Smith</p>
+            </div>
+        </section>
+        <section className='contact'>
+            <h2>Contact-us</h2>
+            <form>
+                <input type='text' placeholder='name'/>
+                <input type='email' placeholder='Email'/>
+                <textarea placeholder='Message'></textarea>
+                <button className='submit-buuton'>send</button>
+            </form>
+        </section>
+        <section>
+            <Footer/>
+        </section>
+        
+
         </div>
     );
 };
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Footer = ()=> {
+    return(
+        <div>
+            <footer>
+            <p>&copy; Event Planner Organization. All rights reserved.</p>  
+            </footer>
+        </div>
+
+    )
+}
+export default Footer;
